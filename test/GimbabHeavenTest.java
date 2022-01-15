a791153de18fa0a1b96dd5ca950b036d5c770823
Jude <legagain@gmail.com>
2022-01-15 23:15:38 +0900

    TestCommit

---
+++ test/GimbabHeavenTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GimbabHeavenTest {
+
+    @Test
+    void abcd() {
+        System.out.println("안녕하세요");
+    }
+
+}
